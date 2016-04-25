8e0ea4b4176f0752a3f2da14182c75eb78d23b2c
Vinicius Morais <vini.m.ribeiro@gmail.com>
2016-04-25 18:10:24 -0300

    Comitando issues #14 e #15

---
+++ FleetTracking/src/Visao/Janelas/Componentes/Campos/JLabelEditData.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Visao.Janelas.Componentes.Campos;
+
+import java.text.ParseException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author vinicius
+ */
+public class JLabelEditData extends JLabelEdit {
+    
+    public JLabelEditData(String caption, int left, int top, int width) throws ParseException {
+        super(caption, left, top, width, true, false, null, null);
+        super.setMask("##/##/####");
+
+        super.campo.addFocusListener(new java.awt.event.FocusAdapter() {
+            @Override
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                if (true/*!ValidaData()*/){
+                    JOptionPane.showMessageDialog(null, "Atenção! Data inválido.");
+                }
+            }
+        });
+    }
+}
