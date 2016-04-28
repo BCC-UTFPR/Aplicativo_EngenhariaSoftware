7d19816c4ca99cf38bab761377108b8d3c95f221
Vinicius Morais <vini.m.ribeiro@gmail.com>
2016-04-28 16:07:00 -0300

    Commitando issue #19

---
+++ FleetTracking/src/Visao/Janelas/Componentes/Campos/JLabelEditStringComConsulta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Visao.Janelas.Componentes.Campos;
+
+/**
+ *
+ * @author vinicius
+ */
+public class JLabelEditStringComConsulta extends JLabelEdit {
+
+    public JLabelEditStringComConsulta(String caption, int left, int top, int width, String table, String pes) throws ParseException {
+        super(caption, left, top, width, false, true, table, pes);
+    }
+}
