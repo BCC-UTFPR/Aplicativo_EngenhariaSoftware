016ac1c2c29b7b6343abb87a6d4c020c0269854f
fronchetti <fronchetti@localhost.localdomain>
2016-05-17 11:04:39 -0300

    #issue 36: solved!

---
+++ Website/login.php
@@ -0,0 +1,36 @@
+<?php 
+
+/* Informações do Banco de Dados*/
+$host = "";
+$username = "";
+$password = "";
+$database = "";
+$table = "";
+
+/* Autenticação */
+mysql_connect("$host","$username","$password") or die ("Impossível conectar!");
+mysql_select_db("$database") or die ("Database inválida. Tente novamente.");
+
+$field_username = $_POST['field_user'];
+$field_password = $_POST['field_password'];
+
+$field_username = stripslashes($field_username);
+$field_password = stripslashes($field_password);
+$field_username = mysql_real_escape_string($field_username);
+$field_password = mysql_real_escape_string($field_password);
+
+$mysql_code = "SELECT * FROM $table WHERE username = '$field_username' AND password = '$field_password'";
+$query = mysql_query($mysql_code);
+ 
+$number_of_results = mysql_num_rows($query);
+
+if ($number_of_results > 0) {
+		session_register("field_username");
+		session_register("field_password");
+}
+
+else {
+	echo "Login incorreto! Tente novamente!";
+}
+
+?>
